ac8354d6f93f30b32c716d7f8dd153aede691666
Steven Blucher <stevenb@macbook-pro.cedarcollege.local>
2020-02-27 12:42:47 +1030

    sign out button made and activated

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,7 @@
-//Signup form - begin by getting a ref to the form
+//references to the HTML elements so they can be manipulated by JS
 const signupForm = document.querySelector('#signup-form');
-
+const logout = document.querySelector('#logout');
+//signing up users
 signupForm.addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -15,3 +16,10 @@ signupForm.addEventListener('submit', function(e) {
     signupForm.reset();
   });
 });
+
+//logging users out
+logout.addEventListener('click', function(e) {
+  e.preventDefault();
+  auth.signOut();
+  console.log('signed out');
+})
