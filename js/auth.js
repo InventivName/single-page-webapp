6ec58ad3d36233c3496d18e27bc2fbc620ee80d2
Steven Blucher <stevenb@macbook-pro.cedarcollege.local>
2020-02-27 12:59:58 +1030

    login function created

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,9 @@
-//Signup form - begin by getting a ref to the form
+//references to the HTML elements so they can be manipulated by JS
 const signupForm = document.querySelector('#signup-form');
+const logout = document.querySelector('#logout');
+const loginForm = document.querySelector('#login-form');
 
+//signing up users
 signupForm.addEventListener('submit', function(e) {
   e.preventDefault();
 
@@ -8,10 +11,31 @@ signupForm.addEventListener('submit', function(e) {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-pass'].value;
 
-  //console.log(email, password);
+  console.log(email, password);
 
   //sign up the user with an email and password to firestore
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
     signupForm.reset();
+  });
+});
+
+//logging users out
+logout.addEventListener('click', function(e) {
+  e.preventDefault();
+  auth.signOut();
+  console.log('signed out');
+});
+
+//logging existing users in
+loginForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-pass'].value;
+
+  //sign the user in with an existing email and password
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    loginForm.reset();
+    console.log('User with email: ' + email + ' signed in');
   });
 });
